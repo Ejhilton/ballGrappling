533c75b10982ab1af5346dbbc388174f2adc0811
zlac212 <zlac212@live.rhul.ac.uk>
2024-06-15 13:26:12 +0100

    Added basic pygame window

--- python/main.py
+++ python/main.py
@@ -1,1 +1,37 @@
-print("Hello")
+from engine import *
+
+pygame.init()
+
+#window info
+useScreen = True
+screenWidth, screenHeight = pygame.display.Info().current_w, pygame.display.Info().current_h
+width = 1000
+height = 1000
+window = Window(width,height,0, "Practice project")
+if useScreen:
+    window = Window(screenWidth, screenHeight, pygame.FULLSCREEN, "Practice project")
+    width = screenWidth
+    height = screenHeight
+
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # input
+
+    # update
+
+    # render
+
+
+    window.clear()
+    window.swapBuffers()
+
+    # frame rate
+    clock.tick(60)
+
+pygame.quit()
